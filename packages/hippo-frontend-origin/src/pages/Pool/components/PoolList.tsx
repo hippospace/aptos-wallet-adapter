2bb9fdd83a2f5c4ece028c83aef51aa408f5a36c
tom <tom@hippo.space>
2022-06-21 12:07:13 +0800

    HIP-31: fix scroll bar

--- packages/hippo-frontend-origin/src/pages/Pool/components/PoolList.tsx
+++ packages/hippo-frontend-origin/src/pages/Pool/components/PoolList.tsx
@@ -1,9 +1,9 @@
 import { List } from 'antd';
 import { IPool } from 'types/pool';
-import VirtualList from 'rc-virtual-list';
+// import VirtualList from 'rc-virtual-list';
 import PoolCard from './PoolCard';
-import { useSelector } from 'react-redux';
-import { getLayoutHeight } from 'modules/common/reducer';
+// import { useSelector } from 'react-redux';
+// import { getLayoutHeight } from 'modules/common/reducer';
 import DepositModal from './DepositModal';
 import { useState } from 'react';
 import WithdrawModal from './WithdrawModal';
@@ -12,7 +12,7 @@ interface TProps {
 }
 
 const PoolList: React.FC<TProps> = ({ filteredPools }) => {
-  const containerHeight = useSelector(getLayoutHeight);
+  // const containerHeight = useSelector(getLayoutHeight);
   const [depositPair, setDepositPair] = useState<IPool>();
   const [withdrawPair, setWithdrawPair] = useState<IPool>();
 
@@ -26,21 +26,15 @@ const PoolList: React.FC<TProps> = ({ filteredPools }) => {
   return (
     <>
       <List className="h-full overflow-y-scroll no-scrollbar border-0">
-        <VirtualList
-          data={filteredPools}
-          height={containerHeight - 100}
-          itemHeight={130}
-          itemKey="tokenList">
-          {(item) => (
-            <List.Item className="!border-b-0 !px-0 !pr-2" key={item.id}>
-              <PoolCard
-                pool={item}
-                onDeposit={() => handleOnClickDeposit(item)}
-                onWithdraw={() => handleOnClickWithdraw(item)}
-              />
-            </List.Item>
-          )}
-        </VirtualList>
+        {filteredPools.map((item) => (
+          <List.Item className="!border-b-0 !px-0 !pr-2" key={item.id}>
+            <PoolCard
+              pool={item}
+              onDeposit={() => handleOnClickDeposit(item)}
+              onWithdraw={() => handleOnClickWithdraw(item)}
+            />
+          </List.Item>
+        ))}
       </List>
       <DepositModal tokenPair={depositPair} onDismissModal={() => setDepositPair(undefined)} />
       <WithdrawModal tokenPair={withdrawPair} onDismissModal={() => setWithdrawPair(undefined)} />
