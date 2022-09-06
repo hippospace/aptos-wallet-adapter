f2ba2c7457fc58de03a4f5e321fcc91d9f9e47f4
tom <tom@hippo.space>
2022-09-06 10:41:31 +1000

    update: add facuet to next js tester

--- packages/wallet-tester/src/config/aptosConstants.ts
+++ packages/wallet-tester/src/config/aptosConstants.ts
@@ -3,5 +3,5 @@ export const DEVNET_NODE_URL = 'https://fullnode.devnet.aptoslabs.com/v1';
 export const LOCAL_FAUCET_URL = 'http://127.0.0.1:8081';
 export const DEVNET_FAUCET_URL = 'https://faucet.devnet.aptoslabs.com';
 
-export const NODE_URL = LOCAL_NODE_URL;
-export const FAUCET_URL = LOCAL_FAUCET_URL;
+export const NODE_URL = DEVNET_NODE_URL;
+export const FAUCET_URL = DEVNET_FAUCET_URL;
