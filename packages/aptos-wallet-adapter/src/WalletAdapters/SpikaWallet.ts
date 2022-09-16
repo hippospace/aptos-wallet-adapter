021c345668d9d9a0f9022a4e1a0de1f630ffa6bf
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-16 04:40:51 +0300

    Remove auto disconnect function in connect()

--- packages/aptos-wallet-adapter/src/WalletAdapters/SpikaWallet.ts
+++ packages/aptos-wallet-adapter/src/WalletAdapters/SpikaWallet.ts
@@ -121,10 +121,11 @@ export class SpikaWalletAdapter extends BaseWalletAdapter {
       this._connecting = true;
 
       const provider = this._provider || window.spika;
-      const isConnected = await provider?.isConnected();
-      if (isConnected === true) {
-        await provider?.disconnect();
-      }
+      await provider?.isConnected();
+      // const isConnected = await provider?.isConnected();
+      // if (isConnected === true) {
+      //   await provider?.disconnect();
+      // }
 
       const response = await provider?.connect();
       if (response?.publicKey !== undefined) {
